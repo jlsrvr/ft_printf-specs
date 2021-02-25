420a60a934c3d5d4daa7facfb4e67b3688730ab5
Jules Rivoire <julesriv@gmail.com>
2021-02-25 20:04:41 +0100

    Add tests for unsigned converter

---
+++ specs/unsigned_converter_spec.c
@@ -0,0 +1,59 @@
+#include "spec.h"
+
+static void	check_result(char *result, char *expected)
+{
+	if (strcmp(result, expected))
+	{
+		printf(RED "KO\n" RESET);
+		printf("Expected = [%s]\n", expected);
+		printf("Got      = [%s]\n", result);
+		return ;
+	}
+	printf(GREEN "OK\n" RESET);
+}
+
+static void base_converter_test(char *describe, t_specs my_specs, int my_int, char *expected)
+{
+	char *result;
+
+	printf("<==== %s ====>\n", describe);
+	result = unsigned_converter(my_specs, my_int);
+	check_result(result, expected);
+	free(result);
+	return ;
+}
+
+char    *base_10 = "0123456789";
+int main(void)
+{
+	char *expected;
+	intmax_t my_int;
+	printf("\\\\\\ Specs unsigned converter ///\n");
+	printf(UNDER"### Negative int ###\n"RESET);
+	my_int = -53;
+	expected = "4294967243";
+	base_converter_test("No specs", test_specs(1, 0, 0, 0, -1, 'u'), my_int, expected);
+	base_converter_test("Only precision 0", test_specs(1, 0, 0, 0, 0, 'u'), my_int, expected);
+	base_converter_test("Only precision 5", test_specs(1, 0, 0, 0, 5, 'u'), my_int, expected);
+	expected = "          4294967243";
+	base_converter_test("Only min-f-width 20", test_specs(1, 0, 0, 20, -1, 'u'), my_int, expected);
+	expected = "     000004294967243";
+	base_converter_test("min-f-width 20 and precision 15", test_specs(1, 0, 0, 20, 15, 'u'), my_int, expected);
+	base_converter_test("Zero-pad, min-f-width 20 and precision 15", test_specs(1, 1, 0, 20, 15, 'u'), my_int, expected);
+	expected = "000004294967243     ";
+	base_converter_test("Right-pad, min-f-width 20 and precision 15", test_specs(1, 0, 1, 20, 15, 'u'), my_int, expected);
+	expected = "000004294967243";
+	base_converter_test("Only precision 15", test_specs(1, 0, 0, 0, 15, 'u'), my_int, expected);
+	expected = "00000000004294967243";
+	base_converter_test("Zero-pad, min-f-width 20", test_specs(1, 1, 0, 20, -1, 'u'), my_int, expected);
+	printf(UNDER"### More than max int ###\n"RESET);
+	my_int = 12345654321234;
+	expected = "1918312530";
+	base_converter_test("No specs", test_specs(1, 0, 0, 0, -1, 'u'), my_int, expected);
+	printf(UNDER"### 0 ###\n"RESET);
+	my_int = 0;
+	expected = "";
+	base_converter_test("Precision 0", test_specs(1, 0, 0, 0, 0, 'u'), my_int, expected);
+
+	return (1);
+}
---
+++ specs/unsigned_converter_spec.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gcc -Wall -Wextra -o unsigned_converter_spec spec_utils.c ../srcs/printf_utils.c ../srcs/string_writer.c ../srcs/converters/*.c unsigned_converter_spec.c -I. -I../srcs/headers -I../srcs/libft/headers -L../srcs/libft -lft && valgrind ./unsigned_converter_spec
