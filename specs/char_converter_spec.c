a7c7623e0e53d7430847ffbe5dadb247e536925c
Jules Rivoire <julesriv@gmail.com>
2021-02-26 17:22:09 +0100

    updated tests for char

--- specs/char_converter_spec.c
+++ specs/char_converter_spec.c
@@ -11,24 +11,6 @@ static void char_converter_test(char *describe, t_specs my_specs, char c, char *
 	return ;
 }
 
-/*static void percentage_converter_test(char *describe, t_specs my_specs, char *expected)
-{
-	char *result;
-
-	printf("<==== %s ====>\n", describe);
-	result = percentage_converter(my_specs, '%');
-	if (strcmp(result, expected))
-	{
-		printf(RED "KO\n" RESET);
-		printf("Excpected = [%s]\n", expected);
-		printf("Got       = [%s]\n", result);
-		return ;
-	}
-	printf(GREEN "OK\n" RESET);
-	free(result);
-	return ;
-}*/
-
 int main(void)
 {
 	int neg_int = -20;
@@ -36,25 +18,25 @@ int main(void)
 	special[0] = (unsigned char)neg_int;
 	special[1] = 0;
 	printf("\\\\\\ Specs char converter ///\n");
-	char_converter_test("No specs", test_specs(1, 0, 0, 0, -1, 0), 'J', "J");
-	char_converter_test("No specs but 0 instead of char", test_specs(1, 0, 0, 0, -1, 0), 0, "\0");
-	char_converter_test("No specs but negative argument", test_specs(1, 0, 0, 0, -1, 0), neg_int, special);
-	char_converter_test("Only min-field-width 5", test_specs(1, 0, 0, 5, -1, 0), 'J', "    J");
-	char_converter_test("Only precision 5", test_specs(1, 0, 0, 0, 5, 0), 'J', "J");
-	char_converter_test("Only min-field-width 100", test_specs(1, 0, 0, 100, -1, 0), 'J', "                                                                                                   J");
-	char_converter_test("Zero padded min-field-width 5", test_specs(1, 1, 0, 5, -1, 0), 'J', "0000J");
-	char_converter_test("Zero padded, min-field-width 5, precision 3", test_specs(1, 1, 0, 5, 3, 0), 'J', "0000J");
-	char_converter_test("Right padded min-field-width 5", test_specs(1, 0, 1, 5, -1, 0), 'J', "J    ");
-	char_converter_test("Right & zero padded min-field-width 5", test_specs(1, 1, 1, 5, -1, 0), 'J', "J    ");
+	char_converter_test("No specs", test_specs(1, 0, 0, 0, -1, 'c'), 'J', "J");
+	char_converter_test("No specs but 0 instead of char", test_specs(1, 0, 0, 0, -1, 'c'), (char)0, '0');
+	char_converter_test("No specs but negative argument", test_specs(1, 0, 0, 0, -1, 'c'), neg_int, special);
+	char_converter_test("Only min-field-width 5", test_specs(1, 0, 0, 5, -1, 'c'), 'J', "    J");
+	char_converter_test("Only precision 5", test_specs(1, 0, 0, 0, 5, 'c'), 'J', "J");
+	char_converter_test("Only min-field-width 100", test_specs(1, 0, 0, 100, -1, 'c'), 'J', "                                                                                                   J");
+	char_converter_test("Zero padded min-field-width 5", test_specs(1, 1, 0, 5, -1, 'c'), 'J', "0000J");
+	char_converter_test("Zero padded, min-field-width 5, precision 3", test_specs(1, 1, 0, 5, 3, 'c'), 'J', "0000J");
+	char_converter_test("Right padded min-field-width 5", test_specs(1, 0, 1, 5, -1, 'c'), 'J', "J    ");
+	char_converter_test("Right & zero padded min-field-width 5", test_specs(1, 1, 1, 5, -1, 'c'), 'J', "J    ");
 	printf("\\\\\\ Specs percentage converter "UNDER"(char function)"RESET" ///\n");
-	char_converter_test("No specs", test_specs(1, 0, 0, 0, -1, 0), '%', "%");
-	char_converter_test("Only min-field-width 5", test_specs(1, 0, 0, 5, -1, 0), '%', "    %");
-	char_converter_test("Only precision 5", test_specs(1, 0, 0, 0, 5, 0), '%', "%");
-	char_converter_test("Only min-field-width 100", test_specs(1, 0, 0, 100, -1, 0), '%', "                                                                                                   %");
-	char_converter_test("Zero padded min-field-width 5", test_specs(1, 1, 0, 5, -1, 0), '%', "0000%");
-	char_converter_test("Zero padded, min-field-width 5, precision 3", test_specs(1, 1, 0, 5, 3, 0), '%', "0000%");
-	char_converter_test("Right padded min-field-width 5", test_specs(1, 0, 1, 5, -1, 0), '%', "%    ");
-	char_converter_test("Right & zero padded min-field-width 5", test_specs(1, 1, 1, 5, -1, 0), '%', "%    ");
+	char_converter_test("No specs", test_specs(1, 0, 0, 0, -1, 'c'), '%', "%");
+	char_converter_test("Only min-field-width 5", test_specs(1, 0, 0, 5, -1, 'c'), '%', "    %");
+	char_converter_test("Only precision 5", test_specs(1, 0, 0, 0, 5, 'c'), '%', "%");
+	char_converter_test("Only min-field-width 100", test_specs(1, 0, 0, 100, -1, 'c'), '%', "                                                                                                   %");
+	char_converter_test("Zero padded min-field-width 5", test_specs(1, 1, 0, 5, -1, 'c'), '%', "0000%");
+	char_converter_test("Zero padded, min-field-width 5, precision 3", test_specs(1, 1, 0, 5, 3, 'c'), '%', "0000%");
+	char_converter_test("Right padded min-field-width 5", test_specs(1, 0, 1, 5, -1, 'c'), '%', "%    ");
+	char_converter_test("Right & zero padded min-field-width 5", test_specs(1, 1, 1, 5, -1, 'c'), '%', "%    ");
 
 	return (1);
 }
