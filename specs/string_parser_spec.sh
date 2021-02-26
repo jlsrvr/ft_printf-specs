614072c14cdcbc15c468f00d8fe5a6eae35d8ff7
Jules Rivoire <julesriv@gmail.com>
2021-02-26 11:23:07 +0100

    Modify tests of string_parser to show need of free

--- specs/string_parser_spec.sh
+++ specs/string_parser_spec.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-gcc -Wall -Wextra -o string_parser_spec spec_utils.c ../srcs/string_parser.c ../srcs/conversion_parser.c ../srcs/converting_function.c ../srcs/printf_utils.c ../srcs/converters/*.c ../srcs/string_writer.c string_parser_spec.c -I. -I../srcs/headers -I../srcs/libft/headers -L../srcs/libft -lft && valgrind ./string_parser_spec
+gcc -Wall -Wextra -o string_parser_spec spec_utils.c ../srcs/string_parser.c ../srcs/conversion_parser.c ../srcs/converting_function.c ../srcs/utils/*.c ../srcs/converters/*.c ../srcs/string_writer.c string_parser_spec.c -I. -I../srcs/headers && valgrind ./string_parser_spec
+rm -rf string_parser_spec string_parser_spec.dSYM/
